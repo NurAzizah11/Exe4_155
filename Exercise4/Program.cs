2fb1031dbd26355216bbd38a9d4650b39ae94927
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-07 15:48:12 +0700

    menambahkan public void pop

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -40,6 +40,12 @@ namespace Exercise4
             top = fresh;
             Console.WriteLine("\n" + element + " pushed.");
         }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node is sequence
+        }
     }
 
         }
