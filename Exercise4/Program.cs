16773685dfb950799c7670269ffb98cdffb83ae5
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-07 15:45:18 +0700

    menambahkan Node fresh di public void

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -23,7 +23,23 @@ namespace Exercise4
             top = null;
         }
 
-        
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
     }
 
         }
